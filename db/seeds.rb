e9aaf0d59b3503ccc3e9efc864180ff9347eccda
LYC <newvry@hotmail.com>
2017-04-12 17:43:35 +0800

    Build: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.destroy_all
+
+course_list = [
+  {"title" => "初級", "description" => "第一關"},
+  {"title" => "中級", "description" => "第二關"},
+  {"title" => "高級", "description" => "第三關"}
+]
+
+
+course_list.each do |course|
+  Course.create( :title => course["title"], :description => course["description"] )
+end
+
+
+lesson_list = [
+  {"title" => "導覽簡介與認識旅客", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.first.id},
+  {"title" => "旅程中的溝通與關懷技巧", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.first.id},
+  {"title" => "旅遊常見問題解決與應對", "description" => "", "course_id" => Course.first.id},
+  {"title" => "歷史", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.find_by(title: "中級").id},
+  {"title" => "飲食", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.find_by(title: "中級").id},
+  {"title" => "建築", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.find_by(title: "中級").id},
+  {"title" => "主題式瀏覽", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.find_by(title: "中級").id},
+  {"title" => "生活策展學", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.last.id},
+  {"title" => "演講學", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.last.id},
+  {"title" => "創作應用", "description" => "Lorem ipsum dolor sit amet.", "course_id" => Course.last.id}
+]
+
+lesson_list.each do |lesson|
+  Lesson.create( :title => lesson["title"], :description => lesson["description"], :course_id => lesson["course_id"] )
+end
+
