008324b061be77eba1a2c351d0c000f28e2d2b1c
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-04-13 11:44:32 +0800

    admin course C.U.D.

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -2,8 +2,37 @@ class Admin::CoursesController < ApplicationController
   before_action :authenticate_user! 
   before_action :authenticate_admin
 
+  before_action :find_course, only: [ :destroy]
+
   def index
     @courses = Course.all
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.save
+    redirect_to admin_courses_path
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to admin_courses_path
+  end
+
+
+  private
+
+  def find_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
+  
 
 end
