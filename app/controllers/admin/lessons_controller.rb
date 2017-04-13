6238a43d2857d44a67e1a567a272a999369cc17d
LYC <newvry@hotmail.com>
2017-04-13 11:20:59 +0800

    feature: setup admin/lesson new and create

--- app/controllers/admin/lessons_controller.rb
+++ app/controllers/admin/lessons_controller.rb
@@ -4,4 +4,24 @@ class Admin::LessonsController < ApplicationController
     @lessons = Lesson.all
   end
 
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to admin_lessons_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :description)
+  end
+
 end
