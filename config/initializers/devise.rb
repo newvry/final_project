07ebbaabe523de6b866d3acb695d8b3d27586473
LYC <newvry@hotmail.com>
2017-04-23 13:58:38 +0800

    test fb sign in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,6 +275,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :facebook, Settings.facebook_app_id, Settings.facebook_secret, :scope => 'public_profile,email', :info_fields => 'email,name', callback_url: "172.104.41.46/users/auth/facebook/callback"
+  config.omniauth :facebook, Settings.facebook_app_id, Settings.facebook_secret, :scope => 'public_profile,email', :info_fields => 'email,name', callback_url: "http://172.104.41.46/users/auth/facebook/callback"
 
 end
