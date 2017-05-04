b19c8121584e120cf6163f3c87964f94c8596336
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-05-04 19:28:37 +0800

    amend js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,62 @@
 //= require fullcalendar
 //= require daterangepicker
 //= require_tree .
+
+$(document).ready(function(){
+
+$("#course_1").css('animation', 'fadeInUp 1s ease-in-out')
+$("#course_2").css({
+  'animation': 'fadeInUp 1s ease-in-out',
+  'animation-delay': '.1s'})
+$("#course_3").css({
+  'animation': 'fadeInUp 1s ease-in-out',
+  'animation-delay': '.2s'})
+
+var current_user = $("#current_user").text()
+
+if (current_user != " "){
+
+  if (current_user == "Entry Level"){
+    $("#course_1").find("i").addClass("fa fa-unlock-alt fa-3x")
+
+    $("#course_2, #course_3").closest("a").click(function(e){
+      e.preventDefault()
+    })
+
+    $("#course_2, #course_3").attr({
+      "data-toggle": "tooltip",
+      "data-placement": "bottom",
+      "title": "You have to finish the previous course to unlock."})
+    $('[data-toggle="tooltip"]').tooltip()
+
+    $("#course_3, #course_2").find("i").addClass("fa fa-lock fa-3x")
+  }else if (current_user.level == "Medium Level"){
+    $("#course_1").find("i").addClass("fa fa-unlock-alt fa-3x")
+
+    $("#course_2").find("i").addClass("fa fa-unlock-alt fa-3x")
+
+    $("#course_3").closest("a").click(function(e){
+      e.preventDefault()
+    })
+
+    $( "#course_3").attr({
+      "data-toggle": "tooltip",
+      "data-placement": "bottom",
+      "title": "You have to finish the previous course to unlock."})
+    $('[data-toggle="tooltip"]').tooltip()
+
+    $("#course_3").find("i").addClass("fa fa-lock fa-3x")
+  }
+}else{
+  $(".access").addClass("fa fa-lock fa-3x")
+  $(".row").find("a").click(function(e){
+    e.preventDefault()
+  })
+
+  $("#course_1, #course_2, #course_3").attr({
+    "data-toggle": "tooltip",
+    "data-placement": "bottom",
+    "title": "Login to start your learning."})
+  $('[data-toggle="tooltip"]').tooltip()
+}
+})
