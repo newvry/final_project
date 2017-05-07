bfc6719c1d0f5cda6911e43a2355f2875e993ce6
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-05-07 15:42:19 +0800

    amend view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506074315) do
+ActiveRecord::Schema.define(version: 20170507065417) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(version: 20170506074315) do
     t.string   "level",                  default: "Entry Level"
     t.text     "intro"
     t.integer  "mobile_phone"
-    t.integer  "course_id",              default: 1
+    t.integer  "course_id",              default: 4
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["fb_uid"], name: "index_users_on_fb_uid"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
